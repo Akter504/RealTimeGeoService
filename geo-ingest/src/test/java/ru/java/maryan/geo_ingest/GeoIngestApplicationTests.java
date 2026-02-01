6105bde648fe951326162f8df0dba3051bbaeec9
Maryan <maryan.sasha@bk.ru>
2026-02-01 20:40:56 +0300

    Initial commit

---
+++ geo-ingest/src/test/java/ru/java/maryan/geo_ingest/GeoIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.maryan.geo_ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ geo-processor/src/test/java/ru/java/maryan/geo_processor/GeoProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.maryan.geo_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
