b13d2825e63a7a0b953875c9b7805986f605be4c
Maryan <maryan.sasha@bk.ru>
2026-04-03 14:31:59 +0300

    Trigger service implementation

---
+++ geo-processor/src/main/java/ru/java/maryan/geo_processor/constants/GeoProcessorConstants.java
@@ -0,0 +1,10 @@
+package ru.java.maryan.geo_processor.constants;
+
+public final class GeoProcessorConstants {
+    private GeoProcessorConstants() {}
+
+    public static final String LAST_KNOWN_LOCATION_PREFIX = "lkl";
+    public static final String TRIGGER_TYPE_ENTRY = "ENTRY";
+    public static final String TRIGGER_TYPE_EXIT = "EXIT";
+    public static final String GEO_GLOBAL_MAP_KEY = "global:geo:active_users";
+}
